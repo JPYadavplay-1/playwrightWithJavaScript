a36ac858b8ba4338e0993fecca096b51bc1d1a32
jyothiprakash-yadav <y.prakash@opsaway.com>
2025-12-22 12:55:24 +0530

    your message

--- fixtures/facebook.fixtures.js
+++ fixtures/facebook.fixtures.js
@@ -1,6 +1,5 @@
 const { test: base, expect } = require('@playwright/test');
 const { FacebookLoginPage } = require('../pages/facebook/login.page');
-
 const test = base.extend({
   facebookLoginPage: async ({ page }, use) => {
     const fbPage = new FacebookLoginPage(page);
